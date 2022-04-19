ab8a8fefb054bb65cec98a6650c3657f0e10d765
ahmad <ahmadhudaya989@gmail.com>
2022-04-19 10:38:29 +0700

    basdat

---
+++ index.js
@@ -0,0 +1,25 @@
+//luas persegi
+function Persegi(sisi){
+  return sisi * sisi
+}
+console.log(`Luas Persegi adalah : ${(5)}`);
+
+//persegi panjang
+function Panjang(p,t){
+  return p * t
+}
+console.log(`Luas Persegi Panjang adalah : ${(4,2)}`);
+
+//lingkaran
+const luasLingkaran =(r) => {
+  const luas = 3.14 || 22/7
+  return luas * r *r
+}
+console.log(`Luas Lingkaran adalah : ${(11)}`);
+
+//jajar genjang
+const luasJajargenjang = (t) => {
+  const alas =1/2
+  return alas * t
+}
+console.log(`Luas Jajar Genjang adalah : ${(15)}`);
